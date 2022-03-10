1230cea0883b71dc06f82b2bf701a432ba114563
Amit Yadav <ay.puzzler@gmail.com>
2022-03-10 10:32:00 +0530

    added a few vars to track the game status

--- build-blackjack/index.js
+++ build-blackjack/index.js
@@ -2,6 +2,7 @@ let firstCard = 10
 let secondCard = 7
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
   console.log('Do you want to draw a new card?')
@@ -10,6 +11,7 @@ if (sum <= 20) {
   hasBlackJack = true
 } else {
   console.log("You're out of the game!")
+  isAlive = false
 }
 
 console.log(hasBlackJack)

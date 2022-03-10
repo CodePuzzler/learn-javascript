7f0daafd12e4681582e5ec245040781ddcd3233d
Amit Yadav <ay.puzzler@gmail.com>
2022-03-10 11:30:20 +0530

    completed blackjack functionality

--- build-blackjack/index.js
+++ build-blackjack/index.js
@@ -1,26 +1,64 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard
+let player = {
+  name: 'Alice',
+  chips: 200,
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = true
 let message = ''
+let messageEL = document.getElementById('message-el')
+let sumEL = document.getElementById('sum-el')
+let cardsEL = document.getElementById('cards-el')
+let playerEL = document.getElementById('player-el')
 
-if (sum <= 20) {
-  message = 'Do you want to draw a new card?'
-} else if (sum === 21) {
-  message = "You've got Blackjack!"
-  hasBlackJack = true
-} else {
-  message = "You're out of the game!"
-  isAlive = false
+playerEL.textContent = player.name + ': $' + player.chips
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
 }
 
-console.log(message)
-
 function startGame() {
-  console.log('start game clicked')
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame() {
+  cardsEL.textContent = 'Cards: '
+  for (let i = 0; i < cards.length; i++) {
+    cardsEL.textContent += cards[i] + ''
+  }
+
+  sumEL.textContent = 'Sum: ' + sum
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?'
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEL.textContent = message
 }
 
 function newCard() {
-  console.log('new card clicked')
+  if (isAlive === true && hasBlackJack == false) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+  }
 }

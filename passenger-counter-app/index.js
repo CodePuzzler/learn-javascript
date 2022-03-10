62eab2ad053ccef5bbe27e36c7b71e50146be738
Amit Yadav <ay.puzzler@gmail.com>
2022-03-10 07:40:18 +0530

    created a variable

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,1 +1,7 @@
-document.getElementById('count-el').innerHTML = 5
+// document.getElementById('count-el').innerHTML = 5
+
+// var to store total passenger count
+let count = 0
+
+// use console.log to debug using browser console
+console.log(count)

932505e3a944c09cebc14a024675cc84d2b3356e
Amit Yadav <ay.puzzler@gmail.com>
2022-03-10 13:13:06 +0530

    added event lsteners to buttons

---
+++ chrome-extension/index.js
@@ -0,0 +1,15 @@
+let inputBTN = document.getElementById('input-btn')
+let deleteBTN = document.getElementById('delete-btn')
+let tabBTN = document.getElementById('tab-btn')
+
+tabBTN.addEventListener('click', function () {
+  console.log('Tab button clicked')
+})
+
+deleteBTN.addEventListener('click', function () {
+  console.log('Delete button clicked')
+})
+
+inputBTN.addEventListener('click', function () {
+  console.log('Button clicked')
+})
